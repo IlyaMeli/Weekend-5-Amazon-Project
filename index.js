f07155e60d535216153e7fa3799cd48a98a30f08
IlyaMeli <ilyameli93@gmail.com>
2021-11-27 22:03:01 +0200

    done web version

---
+++ index.js
@@ -0,0 +1,262 @@
+let cards = [
+  {
+    img: "./essets/images/horse.jpg",
+    alt: "horse",
+    cardInfo: "Super horse finger toy",
+    cPrice: "$9:95",
+    day: "Tuesday",
+    dDay: "Dec 21",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/fenrici.jpg",
+    alt: "februcu",
+    cardInfo:
+      "Fenrici Snapper Fidget Toy for Kids, Adults, Dual Auditory and Sensory Stress Relief, Hand Grip Suction Cup for…",
+    cPrice: "$14:99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-9.jpg",
+    alt: "februcu",
+    cardInfo:
+      "Fenrici Snapper Fidget Toy for Kids, Adults, Dual Auditory and Sensory Stress Relief, Hand Grip Suction Cup for…",
+    cPrice: "$14:99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-11.jpg",
+    alt: "februcu",
+    cardInfo:
+      "Fenrici Snapper Fidget Toy for Kids, Adults, Dual Auditory anp Suction Cup for…",
+    cPrice: "$14.99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-1.jpg",
+    alt: "februcu",
+    cardInfo: "Random content for this item, item number 3",
+    cPrice: "$4.99",
+    day: "Monday",
+    dDay: "Dec 29",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/fenrici.jpg",
+    alt: "februcu",
+    cardInfo: "Fenrici Snapper Fidget Toy for Kids, d Grip Suction Cup for…",
+    cPrice: "$14.99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/fenrici.jpg",
+    alt: "februcu",
+    cardInfo:
+      "Fenrici Snapper Fidget Toy for Kids, Adults, Dual Auditory and Sen",
+    cPrice: "$1495.99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-1.jpg",
+    alt: "februcu",
+    cardInfo: "Random content for this item, item number 3",
+    cPrice: "$4.99",
+    day: "Monday",
+    dDay: "Dec 29",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-2.jpg",
+    alt: "februcu",
+    cardInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elitdidunt ",
+    cPrice: "$14.99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-3.jpg",
+    alt: "februcu",
+    cardInfo:
+      "Lorem ipsum dolor sit amet, consectetur  sed do eiusmod tempor incididunt ",
+    cPrice: "$14.99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-4.jpg",
+    alt: "februcu",
+    cardInfo:
+      "adipiscing elit, adipiscing elit, sed do elit, sed do eiusmod tempor incididunt ",
+    cPrice: "$54.99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-5.jpg",
+    alt: "februcu",
+    cardInfo:
+      "Fenrici Snapper Fidget Toy for Kids, Adults, DuaGrip Suction Cup for…",
+    cPrice: "$14.99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-1.jpg",
+    alt: "februcu",
+    cardInfo: "Random content for this item, item number 3",
+    cPrice: "$4.99",
+    day: "Monday",
+    dDay: "Dec 29",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-1.jpg",
+    alt: "februcu",
+    cardInfo: "Random content for this item, item number 3",
+    cPrice: "$4:99",
+    day: "Monday",
+    dDay: "Dec 29",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-6.jpg",
+    alt: "februcu",
+    cardInfo: "Random content for this item, item number 3",
+    cPrice: "$4:99",
+    day: "Monday",
+    dDay: "Dec 29",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-10.jpg",
+    alt: "februcu",
+    cardInfo: "Random content for this item, item number 3",
+    cPrice: "$4:99",
+    day: "Monday",
+    dDay: "Dec 29",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-8.jpg",
+    alt: "februcu",
+    cardInfo: "Random content for this item, item number 3",
+    cPrice: "$4:99",
+    day: "Monday",
+    dDay: "Dec 29",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/fenrici.jpg",
+    alt: "februcu",
+    cardInfo:
+      "Fel Auditory and Sensory Stress Relief, Hand Grip Suction Cup for…",
+    cPrice: "$14:99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/fenrici.jpg",
+    alt: "februcu",
+    cardInfo:
+      "Fenrici Snapper Fidget Toy for Kidlief, Hand Grip Suction Cup for…",
+    cPrice: "$14:99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-3.jpg",
+    alt: "februcu",
+    cardInfo: "Fenrici Snapper Fidget Tction Cup for…",
+    cPrice: "$14:99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+  {
+    img: "./essets/images/item-5.jpg",
+    alt: "februcu",
+    cardInfo: "Fenrici Snapper Fidget Toy for Kids for…",
+    cPrice: "$14:99",
+    day: "Tuesday",
+    dDay: "Dec 20",
+    bInfo: `FREE Shipping on orders over $49 shipped by Amazon
+    Only 5 left in stock - order...`,
+  },
+];
+
+let cardsContainer = document.querySelector(".carousel-content");
+let carousel = document.querySelector(".carousel-content");
+let rightClick = document.querySelector(".fas.fa-chevron-right");
+let leftClick = document.querySelector(".fas.fa-chevron-left");
+
+rightClick.addEventListener("click", () => {
+  cardsContainer.scrollBy(carousel.offsetWidth, 0);
+});
+leftClick.addEventListener("click", () => {
+  cardsContainer.scrollBy(-carousel.offsetWidth, 0);
+});
+let smallPic1 = document.querySelector(".pic-1");
+let smallPic2 = document.querySelector(".pic-2");
+let backPic = document.querySelector(".content-2");
+
+smallPic1.addEventListener("mouseover", () => {
+  backPic.style.backgroundImage = "url('../essets/images/hero.jpg') ";
+  backPic.style.backgroundSize = `100%`;
+});
+
+smallPic2.addEventListener("mouseover", () => {
+  backPic.style.backgroundImage = `url("../essets/images/hero-img-2.jpg")`;
+  backPic.style.backgroundSize = `70%`;
+});
+
+let addDinamicly = (cardsArray) => {
+  for (let i = 0; cardsArray.length; i++) {
+    cardsContainer.innerHTML += `<div class="carousel-card fx fdc">
+<img class="cp" src=${cardsArray[i].img} alt=${cardsArray[i].alt} />
+<div class="card-info">${cardsArray[i].cardInfo}</div>
+<div class="stars"></div>
+<div class="c-price">${cardsArray[i].cPrice}</div>
+<div class="get-it">get it as soon as <span class="day">${cardsArray[i].day}</span></div>
+<div class="d-day">${cardsArray[i].dDay}</div>
+<div class="b-info">${cardsArray[i].bInfo}</div>
+</div>`;
+  }
+};
+
+addDinamicly(cards);
